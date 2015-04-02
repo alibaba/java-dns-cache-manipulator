471ee76256f8c9b815bbbb59331ff8fd1593a77b
Jerry Lee <oldratlee@gmail.com>
2015-04-02 23:18:02 +0800

    add InetAddressCacheUtilTest

--- src/test/java/com/alibaba/dcm/internal/InetAddressCacheUtilTest.java
+++ src/test/java/com/alibaba/dcm/internal/InetAddressCacheUtilTest.java
@@ -1,8 +1,18 @@
 package com.alibaba.dcm.internal;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * @author ding.lid
  */
 public class InetAddressCacheUtilTest {
-
+    @Test
+    public void test_ip2ByteArray() throws Exception {
+        assertArrayEquals(new byte[]{1, 1, 1, 1},
+                InetAddressCacheUtil.ip2ByteArray("1.1.1.1"));
+        assertArrayEquals(new byte[]{10, (byte) 192, (byte) 255, 0},
+                InetAddressCacheUtil.ip2ByteArray("10.192.255.0"));
+    }
 }
