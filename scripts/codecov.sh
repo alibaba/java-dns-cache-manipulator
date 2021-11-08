6205d833dfe857ff55be727ebed4cd671d2e1057
Jerry Lee <oldratlee@gmail.com>
2021-11-09 01:52:09 +0800

    ! fix jacoco-maven-plugin:report do not append append

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -5,7 +5,15 @@ cd "$(dirname "$(readlink -f "$0")")"
 source ./common_build.sh
 source ./prepare-jdk.sh
 
-switch_to_jdk 11
-
 cd "$ROOT_PROJECT_DIR"
-runCmd ./mvnw -Pgen-code-cov clean test jacoco:report coveralls:report
+
+switch_to_jdk 11
+runCmd "${MVN_CMD[@]}" -Pgen-code-cov clean test jacoco:report
+
+switch_to_jdk 8
+# use -Dmaven.main.skip option fix below problem of jacoco-maven-plugin:report :
+#
+# [WARNING] Classes in bundle 'Java Dns Cache Manipulator(DCM) Lib' do not match with execution data.
+#           For report generation the same class files must be used as at runtime.
+# [WARNING] Execution data for class com/alibaba/xxx/Yyy does not match.
+runCmd "${MVN_CMD[@]}" -Pgen-code-cov -Dmaven.main.skip test jacoco:report coveralls:report
