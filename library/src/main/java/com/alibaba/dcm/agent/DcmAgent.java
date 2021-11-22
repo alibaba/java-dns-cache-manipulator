ede7ecd45ae1aa81990a4c9fff09c89489d072d7
Jerry Lee <oldratlee@gmail.com>
2021-11-22 12:56:40 +0800

    ! refactor: split big method `agentmain`

--- library/src/main/java/com/alibaba/dcm/agent/DcmAgent.java
+++ library/src/main/java/com/alibaba/dcm/agent/DcmAgent.java
@@ -4,10 +4,9 @@ import com.alibaba.dcm.DnsCache;
 import com.alibaba.dcm.DnsCacheEntry;
 import com.alibaba.dcm.DnsCacheManipulator;
 
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.*;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -28,7 +27,7 @@ public class DcmAgent {
 
     static final String DCM_AGENT_SUCCESS_MARK_LINE = "!!DCM SUCCESS!!";
 
-    public static void agentmain(String agentArgument) throws Exception {
+    public static void agentmain(@Nonnull String agentArgument) throws Exception {
         logger.info(format("%s: attached with agent argument: %s.%n", DcmAgent.class.getName(), agentArgument));
 
         agentArgument = agentArgument.trim();
@@ -39,20 +38,12 @@ public class DcmAgent {
 
         initAction2Method();
 
-        FileOutputStream fileOutputStream = null;
+        PrintWriter filePrinter = null;
         try {
             final Map<String, List<String>> action2Arguments = parseAgentArgument(agentArgument);
 
-            PrintWriter filePrinter = null;
-
             // Extract file argument, set file printer if needed
-            if (action2Arguments.containsKey(FILE_KEY)) {
-                fileOutputStream = new FileOutputStream(action2Arguments.get(FILE_KEY).get(0), false);
-                final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
-
-                filePrinter = new PrintWriter(outputStreamWriter, true);
-                action2Arguments.remove(FILE_KEY);
-            }
+            filePrinter = getFilePrintWriter(action2Arguments.remove(FILE_KEY));
 
             if (action2Arguments.isEmpty()) {
                 logger.info(DcmAgent.class.getName() + ": No action in agent argument, do nothing!");
@@ -63,47 +54,21 @@ public class DcmAgent {
             }
 
             boolean allSuccess = true;
-
             for (Map.Entry<String, List<String>> entry : action2Arguments.entrySet()) {
                 final String action = entry.getKey();
                 final List<String> arguments = entry.getValue();
-                final String argumentString = join(arguments);
 
-                if (!action2Method.containsKey(action)) {
-                    logger.info(format(("%s: Unknown action %s, ignore! action: %<s %s!%n"), DcmAgent.class.getName(), action, argumentString));
-                    if (filePrinter != null) {
-                        filePrinter.printf("Unknown action %s, ignore! action: %<s %s !%n", action, argumentString);
-                    }
-                    continue;
-                }
-
-                try {
-                    final Object result = doAction(action, arguments.toArray(new String[0]));
-                    printResult(action, result, filePrinter);
-                } catch (Exception e) {
-                    allSuccess = false;
-                    final String exString = throwable2StackString(e);
-                    final String sdtoutExString;
-                    if (isDcmAgentSupressExceptionStack()) {
-                        sdtoutExString = e.toString();
-                    } else {
-                        sdtoutExString = exString;
-                    }
-
-                    logger.info(format(("%s: Error to do action %s %s, cause: %s%n"), DcmAgent.class.getName(), action, argumentString, sdtoutExString));
-                    if (filePrinter != null) {
-                        filePrinter.printf("Error to do action %s %s, cause: %s%n", action, argumentString, exString);
-                    }
-                }
+                boolean success = doAction(action, arguments, filePrinter);
+                if (!success) allSuccess = false;
             }
 
             if (allSuccess && filePrinter != null) {
                 filePrinter.println(DCM_AGENT_SUCCESS_MARK_LINE);
             }
         } finally {
-            if (fileOutputStream != null) {
+            if (filePrinter != null) {
                 try {
-                    fileOutputStream.close();
+                    filePrinter.close();
                 } catch (Throwable e) {
                     // do nothing!
                 }
@@ -111,7 +76,8 @@ public class DcmAgent {
         }
     }
 
-    private static Map<String, List<String>> parseAgentArgument(String argument) {
+    @Nonnull
+    private static Map<String, List<String>> parseAgentArgument(@Nonnull String argument) {
         final String[] split = argument.split("\\s+");
 
         int idx = 0;
@@ -135,22 +101,60 @@ public class DcmAgent {
         return action2Arguments;
     }
 
-    private static String join(List<String> list) {
-        return join(list, " ");
+    @Nullable
+    private static PrintWriter getFilePrintWriter(@Nullable List<String> files) throws FileNotFoundException, UnsupportedEncodingException {
+        if (null == files) return null;
+
+        FileOutputStream fileOutputStream = new FileOutputStream(files.get(0), false);
+        final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
+
+        return new PrintWriter(outputStreamWriter, true);
     }
 
-    private static String join(List<String> list, String separator) {
-        StringBuilder ret = new StringBuilder();
-        for (String argument : list) {
-            if (ret.length() > 0) {
-                ret.append(separator);
+    private static boolean doAction(final String action, final List<String> arguments, final PrintWriter filePrinter) {
+        final String argumentString = join(arguments);
+
+        if (!action2Method.containsKey(action)) {
+            logger.info(format(("%s: Unknown action %s, ignore! action: %<s %s!%n"), DcmAgent.class.getName(), action, argumentString));
+            if (filePrinter != null) {
+                filePrinter.printf("Unknown action %s, ignore! action: %<s %s !%n", action, argumentString);
             }
-            ret.append(argument);
+            return false;
         }
-        return ret.toString();
+
+        try {
+            final Object result = invokeAction(action, arguments.toArray(new String[0]));
+            printActionResult(action, result, filePrinter);
+            return true;
+        } catch (Exception e) {
+            final String exString = throwable2StackString(e);
+            final String sdtoutExString;
+            if (isDcmAgentSupressExceptionStack()) {
+                sdtoutExString = e.toString();
+            } else {
+                sdtoutExString = exString;
+            }
+
+            logger.info(format(("%s: Error to do action %s %s, cause: %s%n"), DcmAgent.class.getName(), action, argumentString, sdtoutExString));
+            if (filePrinter != null) {
+                filePrinter.printf("Error to do action %s %s, cause: %s%n", action, argumentString, exString);
+            }
+
+            return false;
+        }
     }
 
-    private static Object doAction(String action, String[] arguments) throws Exception {
+    private static boolean isDcmAgentSupressExceptionStack() {
+        String supressException = getConfig(DCM_AGENT_SUPRESS_EXCEPTION_STACK);
+        if (supressException == null) return false;
+
+        supressException = supressException.trim();
+        if (supressException.length() == 0) return false;
+
+        return "true".equalsIgnoreCase(supressException);
+    }
+
+    private static Object invokeAction(String action, String[] arguments) throws Exception {
         Method method = action2Method.get(action);
 
         final Class<?>[] parameterTypes = method.getParameterTypes();
@@ -197,25 +201,7 @@ public class DcmAgent {
         return methodArgs;
     }
 
-    private static boolean isDcmAgentSupressExceptionStack() {
-        String supressException = getConfig(DCM_AGENT_SUPRESS_EXCEPTION_STACK);
-        if (supressException == null) return false;
-
-        supressException = supressException.trim();
-        if (supressException.length() == 0) return false;
-
-        return "true".equalsIgnoreCase(supressException);
-    }
-
-    private static String getConfig(String name) {
-        String var = System.getenv(name);
-        if (var == null || var.trim().length() == 0) {
-            var = System.getProperty(name);
-        }
-        return var;
-    }
-
-    private static void printResult(String action, Object result, PrintWriter writer) {
+    private static void printActionResult(String action, Object result, PrintWriter writer) {
         if (writer == null) return;
 
         final Method method = action2Method.get(action);
@@ -242,12 +228,6 @@ public class DcmAgent {
         } else {
             writer.println(result);
         }
-    }
-
-    private static String throwable2StackString(Throwable e) {
-        final StringWriter w = new StringWriter();
-        e.printStackTrace(new PrintWriter(w, true));
-        return w.toString();
     }
 
     private static volatile Map<String, Method> action2Method;
@@ -286,5 +266,42 @@ public class DcmAgent {
         } catch (Exception e) {
             throw new RuntimeException("fail to getActionList, cause: " + e, e);
         }
+    }
+
+    ///////////////////////////////////////////////
+    // util methods
+    ///////////////////////////////////////////////
+
+    @Nullable
+    private static String getConfig(@Nonnull String name) {
+        String var = System.getenv(name);
+        if (var == null || var.trim().length() == 0) {
+            var = System.getProperty(name);
+        }
+        return var;
+    }
+
+    @Nonnull
+    private static String join(@Nonnull List<String> list) {
+        return join(list, " ");
+    }
+
+    @Nonnull
+    private static String join(@Nonnull List<String> list, @Nonnull String separator) {
+        StringBuilder ret = new StringBuilder();
+        for (String argument : list) {
+            if (ret.length() > 0) {
+                ret.append(separator);
+            }
+            ret.append(argument);
+        }
+        return ret.toString();
+    }
+
+    @Nonnull
+    private static String throwable2StackString(@Nonnull Throwable e) {
+        final StringWriter w = new StringWriter();
+        e.printStackTrace(new PrintWriter(w, true));
+        return w.toString();
     }
 }
