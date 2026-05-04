fc916c1ba422c38e9bf4e6304b8bc858d107c529
Jerry Lee <oldratlee@gmail.com>
2026-05-05 01:30:54 +0800

    add 1.8.4-Alpha1 javadoc

---
+++ apidocs/1.8.4-Alpha1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.alibaba.dcm"},{"l":"com.alibaba.dcm.agent"}]
---
+++ apidocs/1.8.4-Alpha1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.alibaba.dcm.agent","l":"DcmAgent"},{"p":"com.alibaba.dcm","l":"DnsCache"},{"p":"com.alibaba.dcm","l":"DnsCacheEntry"},{"p":"com.alibaba.dcm","l":"DnsCacheManipulator"},{"p":"com.alibaba.dcm","l":"DnsCacheManipulatorException"}]
